7c2e62afc99e0f1894b483b4e535241adc477735
saren.sroeum <sroeumsaren809@gmial.com>
2026-01-14 15:20:22 +0700

    first commit

---
+++ JS/vaiable.js
@@ -0,0 +1,54 @@
+// //var keyword
+// var num = 2007;
+// console.log("num :", num);
+
+// //let keyword
+// let name = "Eim";
+// name = "Eimly";
+// console.log("name : ", name);
+
+// function test(){
+//     //function scope
+//     var test1 = "Using var keyword : it is function scope.";
+//     console.log("Var : ", test1);
+//     //bolck scope
+//     let test2 = "Using let keyword : it is block scope.";
+//     console.log("Let : ", test2);
+// }
+// test();
+ 
+// function Test(){
+//     //function scope with using var
+//     var test3 = "Using var";
+//     console.log("test3 inside function", test3);
+//     //block scope with using let
+//     let test4 = "Using let";
+//     console.log("test4 inside function", test4);
+
+// }
+// Test();
+
+// //function scope with using var
+// var test3 = "Using var";
+// console.log("test3 outside function", test3);
+
+// //block scope with using let
+// let test4 = "Using let";
+// console.log("test4 outside function", test4);
+
+console.clear();
+
+console.log("HELLO");
+
+console.count("Count: ");
+
+// console.error("This is an error");
+
+const user = { name: "Saren", age: 20 };
+console.dir(user);
+
+console.info("Information message");
+
+
+
+
---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("Hello Javascript");
+document.querySelector("body").style.backgroundColor = "lightblue";
+alert("Do not open my page");
